ff0910ea684cc2d7f4861f918b37f4165271b331
nicogreenn <nicogreen1201@gmail.com>
2026-04-28 17:47:31 +0100

    fix missing handleDeleteRecipe

--- components/WorkoutApp.jsx
+++ components/WorkoutApp.jsx
@@ -1111,6 +1111,12 @@ export default function WorkoutApp({ user, onSignOut }) {
     setRecipes(newRecipes);
     saveSettings({ recipes: newRecipes });
   };
+
+  const handleDeleteRecipe = (id) => {
+    const newRecipes = recipes.filter(r => r.id !== id);
+    setRecipes(newRecipes);
+    saveSettings({ recipes: newRecipes });
+  };
     const newRecipes = recipes.filter(r => r.id !== id);
     setRecipes(newRecipes);
     const newLog = todayLog.filter(lid => lid !== id);
